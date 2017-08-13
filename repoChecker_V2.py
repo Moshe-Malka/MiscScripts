eed5d11e24d5b3449b4a8d413fa7a87b7fbbf45d
moshe malka <moshemalka2014@gmail.com>
2017-08-14 02:02:31 +0300

    Add files via upload

---
+++ repoChecker_V2.py
@@ -0,0 +1,16 @@
+import subprocess
+from os import path
+from sys import exit
+import getpass
+base_directory="/Users/" + getpass.getuser() + "/.atom/packages/extraction-debugger/ExtractorsRepo/"
+if (not path.exists(base_directory)):
+    print "[!] Error - Path does not exists. did you typed your username correctly?"
+    print "[!] Erroneous Path : "+base_directory
+    exit(1)
+command = "cd ~/.atom/packages/extraction-debugger/ExtractorsRepo && git pull && git ls-files"
+files_path = subprocess.check_output(command, shell=True).split("\n")
+for filename in files_path:
+    if(path.isfile(base_directory+filename)):
+        with open(base_directory+filename, 'r') as infile:
+            if ("snapshot:" in ''.join(infile.readlines())):
+                print "[#] Found one : "+filename
