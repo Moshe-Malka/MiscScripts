b75c986c2ef31012071805e60661afa10a04de5f
moshe malka <moshemalka2014@gmail.com>
2017-10-02 03:51:12 +0300

    Add files via upload

--- proxy_get.py
+++ proxy_get.py
@@ -7,11 +7,10 @@ m_driver = webdriver.PhantomJS()
 m_driver.set_window_size(1920, 1080)
 
 m_driver.get(main_url)
-ts = time.time()
-t = datetime.datetime.fromtimestamp(ts).strftime('%Y_%m_%d-%H_%M_%S')
+t = datetime.datetime.fromtimestamp(time.time()).strftime('%Y_%m_%d-%H_%M_%S')
 m_driver.save_screenshot("proxy_list_"+str(t)+".png")
 
 proxies_list = m_driver.find_elements_by_css_selector('tr[class^="proxy"]')
 for proxy in proxies_list:
-    print "[#] Proxy : {0}      From : {1}".format(str(proxy.get_attribute("prx")),str(proxy.get_attribute("country")))
+    print "[#] Proxy IP: {0}   Port: {1}   From: {2}".format(proxy.get_attribute("prx").split(":")[0],proxy.get_attribute("prx").split(":")[1],proxy.get_attribute("country"))
     print "-"*100
