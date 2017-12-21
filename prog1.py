eb50fc05eed7a877b67fa23c1386ec058410065b
moshe malka <moshemalka2014@gmail.com>
2017-12-21 18:09:10 +0200

    Add files via upload

--- prog1.py
+++ prog1.py
@@ -1,40 +1,71 @@
 
-import openpyxl
 import sys
 import re
 
-filename = r"C:\Users\User\Desktop\data.xlsx"
+def getWB(filename):
+    try:
+        return openpyxl.load_workbook(filename)
+    except Exception:
+        print "Error reading file."
+        print "File path : {0}".format(filename)
+        sys.exit(1)
 
-# name = input("> Please type the name to extract: ")
-# person = str(name).encode('UTF-8')
-person = r"ערן פייגין".decode('UTF-8')
-out_arr = []
-try:
-    wb = openpyxl.load_workbook(filename)
-except Exception as e:
-    print "Error reading file."
-    sys.exit(1)
-sheet_names = wb.get_sheet_names()
-for sheet in sheet_names:
-    current_sheet = wb.get_sheet_by_name(sheet)
-    ##### get first row #####
-    #TODO
-    ##### get certain person's values according to the person's name #####
-    for row in current_sheet.rows:
+def getSheetHeader(sh):
+    head = []
+    for i in range(1,len(list(sh.columns))+1):
+        val = sh.cell(row=1, column=i).value
+        if(val is not None):
+            head.append(val)
+    head = head[:1]+head[2:]
+    return head
+
+def getValuesByName(person, sheet):
+    values = []
+    for row in sheet.rows:
         if(row[1].value is not None):
             if(row[1].value == person):
-                # getting the full row denoting the person's name
-                # print list(row)[:1]+list(row)[2:]
                 person_row = list(row)[:1]+list(row)[2:]
                 for cell in person_row:
-                    print cell.value
-                # number_of_cols = len(list(row)[1:])
-                # for i in range(number_of_cols):
-                #     curr = list(row)[1:][i].value
-                #     if(curr is not None):
-                #         print curr
-                        # out_arr.append(str(curr))   
-    break
-print out_arr
-    # print sheet.encode('UTF-8')
+                    if (cell.value is not None):
+                        values.append(cell.value)
+    return values
 
+def matchArrays(a, b):
+    return map(None, a, b)
+
+if __name__ == '__main__':
+    try:
+        import openpyxl
+    except ImportError as e:
+        print "[!] Please install openpyxl before running this script."
+        print "pip install openpyxl"
+
+    # filename = r"C:\Users\User\Desktop\data.xlsx"
+    filename = "/Users/moshemalka/Desktop/data.xlsx"
+
+    # getting our main openpyxl obj.
+    wb = getWB(filename)
+
+    # name = input("> Please type the name to extract: ")
+    # person = str(name).encode('UTF-8')
+    person = r"ערן פייגין".decode('UTF-8')
+    matched_arr = []
+
+    sheet_names = wb.get_sheet_names()
+    for sheet in sheet_names:
+        c_data = []
+        current_sheet = wb.get_sheet_by_name(sheet)
+        header = getSheetHeader(current_sheet)
+        row_values = getValuesByName(person, current_sheet)
+        ##TODO: make a function that takes 2 args - sheet name and values (sheet, matched_arr), and prints them out.
+        print "*************" + sheet + "*************"
+        matched_arr.append(matchArrays(header, row_values))
+        # break
+    # print out_arr
+    for x in matched_arr:
+        for y in x:
+            for z in x:
+                print z[0]
+                print z[1]
+                print "["+"-"*50+"]"
+        
