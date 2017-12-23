3358c205b87d4a5a9a03885079592b79515db9c5
moshe malka <moshemalka2014@gmail.com>
2017-12-23 16:38:09 +0200

    Add files via upload

--- prog1.py
+++ prog1.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 
 import sys
 import re
@@ -46,6 +47,24 @@ def pprintResults(sheetName, values):
         else:
             print val[0] + " : -"
 
+def writeTofile(m_person, shName, m_values):
+    with open('output.txt', 'wb') as writer:
+        writer.write(m_person.encode('utf-8'))
+        writer.write(shName.encode('utf-8') + '\n')
+        data = []
+        for val in m_values:
+            if None not in val:
+                if type(val[1]) is float:
+                    data.append(val[0] + " : ")
+                    data.append(str(val[1]).replace(".",""))
+                else:
+                    data.append(val[0] + " : ")
+                    data.append(val[1])
+            else:
+                data.append(val[0] + " : -")
+        data = [x.encode('utf-8') for x in data]
+        writer.writelines(data)
+
 if __name__ == '__main__':
     try:
         import openpyxl
@@ -53,15 +72,12 @@ if __name__ == '__main__':
         print "[!] Please install openpyxl before running this script."
         print "pip install openpyxl"
 
-    # filename = r"C:\Users\User\Desktop\data.xlsx"
-    filename = "/Users/moshemalka/Desktop/data.xlsx"
-
-    # getting our main openpyxl obj.
-    wb = getWorkBook(filename)
-
     # name = input("> Please type the name to extract: ")
     # person = name.encode('UTF-8')
-    person = r"ערן פייגין".decode('UTF-8')
+    person = r"ערן פייגין".encode('UTF-8')
+    filename = "/Users/moshemalka/Desktop/data.xlsx"
+
+    wb = getWorkBook(filename)
 
     sheet_names = wb.get_sheet_names()
     print "Results for : {}".format(person.encode('UTF-8'))
@@ -70,11 +86,10 @@ if __name__ == '__main__':
         headers = getSheetHeaders(current_sheet)
         row_values = getValuesByName(person, current_sheet)
         pprintResults(sheet, matchArrays(headers, row_values))
+        # writeTofile(person, sheet, matchArrays(headers, row_values))
         print "\n"
-
-
-
-# need to fix 2 errors in the xslx file:
+        
+# fixed 2 errors in the xslx file:
 #   in the fax2mail sheet - the name column should be B. as in all other sheets.
 #   i fixed it by moved the name column to the B section.
 #   i also fixed it in some more sheets.
